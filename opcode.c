2bb1e6194060e170a2521ee7cfe342a2e0dc17e6
Fidel Caicedo <869@holbertonschool.com>
2019-09-25 18:58:43 +0000

    adding error segm

--- opcode.c
+++ opcode.c
@@ -10,24 +10,29 @@
 menu *create_opstruct(menu **head, char *opname, int n_args)
 {
 		menu *ptr;
+		menu *tmp;
+		unsigned int cont;
 
 	if (head == NULL)
 		return (NULL);
 	ptr = malloc(sizeof(menu));
 	if (ptr == NULL)
 		return (NULL);
+	ptr->opcode = opname;
+	ptr->n_args = n_args;
+	ptr->next = NULL;
 	if (*head == NULL)
 	{
 		*head = ptr;
-		ptr->opcode = opname;
-		ptr->n_args = n_args;
-		ptr->next = NULL;
+		return (*head);
 	}
-	ptr->next = *head;
-	ptr->opcode = opname;
-	ptr->n_args = n_args;
-	*head = ptr;
-	return (ptr);
+	tmp = *head;
+	for (cont = 0; tmp->next != NULL; cont++)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = ptr;
+	return (*head);
 }
 /**
 * opcode_list - creates the oplist
@@ -38,14 +43,30 @@ menu **opcode_list(void)
 {
 	menu **head;
 
-	*head = NULL;
+	head = malloc(sizeof(menu *) * 1);
+	head = NULL;
 	create_opstruct(head, "push", 1);
-	create_opstruct(head, "pall", 0);
-	create_opstruct(head, "pint", 0);
-	create_opstruct(head, "pop", 0);
-	create_opstruct(head, "swap", 0);
-	create_opstruct(head, "add", 0);
-	create_opstruct(head, "nop", 0);
+	if (*head == NULL)
+		free_opcodelist(*head);
+		return (NULL);
+	if (create_opstruct(head, "pall", 0) == NULL)
+		free_opcodelist(*head);
+		return (NULL);
+	if (create_opstruct(head, "pint", 0) == NULL)
+		free_opcodelist(*head);
+		return (NULL);
+	if (create_opstruct(head, "pop", 0) == NULL)
+		free_opcodelist(*head);
+		return (NULL);
+	if (create_opstruct(head, "swap", 0) == NULL)
+		free_opcodelist(*head);
+		return (NULL);
+	if (create_opstruct(head, "add", 0) == NULL)
+		free_opcodelist(*head);
+		return (NULL);
+	if (create_opstruct(head, "nop", 0) == NULL)
+		free_opcodelist(*head);
+		return (NULL);
 	return (head);
 }
 /**
@@ -63,4 +84,17 @@ void free_opcodelist(menu *head)
 		head = tmp;
 	}
 	head = NULL;
+}
+
+int main (void)
+{
+	menu **opcode;
+	int cont = 0;
+	opcode = opcode_list();
+	for (cont = 0; *opcode != NULL; cont++)
+	{
+		printf("%s is the name and %d",(*opcode)->opcode, (*opcode)->n_args);
+		(*opcode) = (*opcode)->next;
+	}
+	return (0);
 }
