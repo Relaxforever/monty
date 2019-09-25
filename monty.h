3d67448cb261696c33bf6edb21271b9cd038c941
Fidel Caicedo <869@holbertonschool.com>
2019-09-25 21:38:48 +0000

    uploading corrected push and adding the correction to monty.h

--- monty.h
+++ monty.h
@@ -49,7 +49,7 @@ int isa_number(char *token);
 int run_interpreter(char *filename);
 menu *create_opstruct(menu **head, char *opname, int n_args);
 menu **opcode_list(void);
-stack_t *push_numberstack(stack_t **head, const int n);
+stack_t *push_numberstack(stack_t **head, unsigned int line_number);
 int file_processor(FILE *file);
 void free_opcodelist(menu *head);
 int isa_opcode(menu **oplist, char *token);
--- push.c
+++ push.c
@@ -3,33 +3,30 @@
 /**
 *push_numberstack - adds a node at the beginning of the list
 *@head: the head of the node
-*@n: the number that will be added.
+*@line_number: the line number that will be added.
 *Return: the list with the new node
 */
-stack_t *push_numberstack(stack_t **head, const int n)
+void *push_numberstack(stack_t **head, unsigned int line_number)
 {
-		stack_t *ptr;
+	stack_t *ptr;
 
-	if (head == NULL)
-		return (NULL);
 	ptr = malloc(sizeof(stack_t));
 	if (ptr == NULL)
-		print_file_error(MALLOC_FAILURE, NULL);
-	if (*head == NULL)
 	{
-		*head = ptr;
-		ptr->n = n;
-		ptr->next = NULL;
-		ptr->prev = NULL;
-		return (ptr);
+		print_file_error(MALLOC_FAILURE, NULL);
 	}
 	if (*head != NULL)
 	{
 		(*head)->prev = ptr;
+		ptr->next = *head;
+		(*head)->next = NULL;
 	}
-	ptr->next = *head;
+	if (*head == NULL)
+	{
+		*head = ptr;
+		ptr->next = NULL;
+	}
 	ptr->prev = NULL;
-	ptr->n = n;
+	ptr->n = pushval;
 	*head = ptr;
-	return (ptr);
 }
