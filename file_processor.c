98a365f35642a475f7acf505eade58ebf664caa0
Dario Castaño <dario.castano@aim.com>
2019-09-25 14:47:05 -0500

    Add EOF verification on file_processor.c

--- file_processor.c
+++ file_processor.c
@@ -4,18 +4,25 @@
 int file_processor(FILE *file)
 {
 	menu **opcodes;
-	static int pushval = 0;
+	// static int pushval = 0;
 	size_t getline_size = 0;
 	char *line_buffer;
-	int ready = 1;
 
-	while (ready)
+	while (!feof(file))
 	{
 		line_buffer = malloc(1024);
-
-
+		opcodes = opcode_list();
+		if (line_buffer == NULL || opcodes == NULL)
+		{
+			fclose(file);
+			print_file_error(MALLOC_FAILURE, NULL);
+		}
+		getline(&line_buffer, &getline_size, file);
+		printf("%s",line_buffer);
 	}
 
-
-	opcodes = opcode_list();
+	fclose(file);
+	free_opcodelist(*opcodes);
+	free(opcodes);
+	return (0);
 }
