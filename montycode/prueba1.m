4a7f239c2806deefb143972887e0478beab82c2e
Dario Castaño <dario.castano@aim.com>
2019-09-26 02:09:19 -0500

    Update prueba1.m

--- montycode/prueba1.m
+++ montycode/prueba1.m
@@ -1,6 +1,12 @@
 push 1
 push 2
-push 3
+push 3 su mama se casó de tenis
 
 push 4
 pall
+                                              push                                                 -666
+esternocleidomastoideo
+
+pop
+pint
+                       swap
