8989f5d26db3e72ac2b636ba6f13ede637ae971d
Fidel Caicedo <869@holbertonschool.com>
2019-09-26 16:30:56 +0000

    adding pop file

---
+++ pop.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+#include "monty_errors.h"
+#include "operations.h"
+/**
+* _pop - eliminates the index one
+* @stack: the list stack
+* @line_number : the line number
+*/
+void _pop(stack_t **stack, unsigned int line_number);
+{
+	stack_t *tmp;
+
+	if (*stack != NULL)
+	{
+		(void) line_number;
+		tmp = *stack;
+		*stack = tmp->next;
+		(*stack)->prev = NULL;
+		free(tmp);
+	}
+	else
+	{
+		print_stack_error(PINT_STACK_EMPTY, line_number);
+	}
+}
