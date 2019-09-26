285ec9a946c516f92ed7868d0d50d72282cea603
Fidel Caicedo <869@holbertonschool.com>
2019-09-26 16:02:40 +0000

    adding _push reworked and _pall

---
+++ pall.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+#include "monty_errors.h"
+/**
+* _pall - shows all the stack.
+*@head: the list
+*@line_number: the line number.
+*/
+void _pall(stack_t **head, unsigned int line_number)
+{
+	int cont = 0;
+	stack_t *tmp;
+
+	(void) line_number;
+	tmp = *head;
+	if (tmp != NULL)
+	{
+		for (tmp = 0; tmp != NULL; cont++)
+		{
+			printf("%d\n", tmp->n);
+			tmp = tmp->next;
+		}
+	}
+}
--- push.c
+++ push.c
@@ -2,7 +2,7 @@
 #include "monty_errors.h"
 #include "operations.h"
 /**
-*push_numberstack - adds a node at the beginning of the list
+*_push - adds a node at the beginning of the list
 *@head: the head of the node
 *@line_number: the line number that will be added.
 *Return: the list with the new node
@@ -31,25 +31,4 @@ void _push(stack_t **head, unsigned int line_number)
 	ptr->prev = NULL;
 	ptr->n = ctx.line;
 	*head = ptr;
-}
-/**
-* pall_numberstack - shows all the stack.
-*@head: the list
-*@line_number: the line number.
-*/
-void _pall(stack_t **head, unsigned int line_number)
-{
-	int cont = 0;
-	stack_t *tmp;
-
-	(void) line_number;
-	tmp = *head;
-	if (tmp != NULL)
-	{
-		for (tmp = 0; tmp != NULL; cont++)
-		{
-			printf("%d\n", tmp->n);
-			tmp = tmp->next;
-		}
-	}
 }
