5c5abba5c405e825d63a0506779d5ae57d2a1891
Dario Castaño <dario.castano@aim.com>
2019-09-26 15:20:36 -0500

    Fix empty push case on m-files

--- push.c
+++ push.c
@@ -10,24 +10,22 @@
 void _push(stack_t **head, unsigned int line_number)
 {
 	stack_t *ptr;
-
-	if (isa_number(ctx.arg) == 1)
-	{
-		(void) line_number;
-		ptr = malloc(sizeof(stack_t));
-		if (ptr == NULL)
-		{
-			print_file_error(MALLOC_FAILURE, NULL);
-		}
-		ptr->n = atoi(ctx.arg);
-		ptr->next = (*head);
-		ptr->prev = NULL;
-		if (*head != NULL)
-		{
-			(*head)->prev = ptr;
-		}
-		*head = ptr;
-	}
-	else
-		print_arg_error(PUSH_FAILURE, line_number);
+	if (ctx.arg) {
+		if (isa_number(ctx.arg) == 1) {
+			(void) line_number;
+			ptr = malloc(sizeof(stack_t));
+			if (ptr == NULL) {
+				print_file_error(MALLOC_FAILURE, NULL);
+			}
+			ptr->n = atoi(ctx.arg);
+			ptr->next = (*head);
+			ptr->prev = NULL;
+			if (*head != NULL) {
+				(*head)->prev = ptr;
+			}
+			*head = ptr;
+		} else
+			print_arg_error(PUSH_FAILURE, line_number);
+	} else
+		print_arg_error(PUSH_FAILURE,line_number);
 }
